1d3d9310588bcd861f37876f7ad85f0f9e9a18aa
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2025-01-29 15:32:17 +0100

    AI and API integration
    Integrated the SheCodes AI API and created the song-lyric generating prompt.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-
+function displayLyrics(response) {
+  let songLyrics = document.querySelector("#song-lyrics");
+  new Typewriter(songLyrics, {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  }).start();
+}
 
 function generateSongLyrics(event) {
   event.preventDefault();
-  let songLyrics = document.querySelector("#song-lyrics");
-  new Typewriter(songLyrics, {
-    strings:
-      "L'Onore! <br /> Ladri! Voi state ligi all'onor vostro, voi! <br/> Cloache d'ignominia! <br/> - Sir John Falstaff",
-    autoStart: true,
-  }).start();
+  let topicInput = document.querySelector("#topic-input").value;
+  let languageInput = document.querySelector("#language-input").value;
+  api_key = "58e244e95c3e78eb13e0ffotadf7c1b8";
+  context =
+    "You are an expert lyricist and poet, you can write in any language, real or fictional. Write a song in very short verses, add a <br /> between verses, it must be between four and twenty verses in length. All verses must be of similar lengths to fit with the music. The refrain must be in <strong></strong>. Follow user instructions to generate the lyrics. The song must have a title as an <h3 class='song-title'></h3> element at the top. Sign in SheCodes Ai at the end in <em class='signature'></em>";
+  prompt = `User Instructions: Generate song lyrics in ${languageInput} about ${topicInput}.`;
+  api_url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
+  axios.get(api_url).then(displayLyrics);
 }
 
 let form = document.querySelector("form");
 form.addEventListener("submit", generateSongLyrics);
-console.log("Hello, ES Modules!");
