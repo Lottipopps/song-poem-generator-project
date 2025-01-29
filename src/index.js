5fe160fe31d224b8f6c4583f1a5ae99d4db93d39
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2025-01-29 14:28:28 +0100

    Added basic js, installed Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import Typewriter from "typewriter-effect/dist/core";
 
+function generateSongLyrics(event) {
+  event.preventDefault();
+  let songLyrics = document.querySelector("#song-lyrics");
+  new Typewriter(songLyrics, {
+    strings:
+      "L'Onore! <br /> Ladri! Voi state ligi all'onor vostro, voi! <br/> Cloache d'ignominia! <br/> - Sir John Falstaff",
+    autoStart: true,
+  });
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", generateSongLyrics);
