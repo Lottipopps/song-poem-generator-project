aacf295b4244a8942f6a44d3b4867249552c2717
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2025-01-29 16:46:18 +0100

    Improved User experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function displayLyrics(response) {
   let songLyrics = document.querySelector("#song-lyrics");
+  songLyrics.innerHTML = "";
   new Typewriter(songLyrics, {
     strings: response.data.answer,
     autoStart: true,
@@ -10,12 +11,15 @@ function displayLyrics(response) {
 
 function generateSongLyrics(event) {
   event.preventDefault();
-  let topicInput = document.querySelector("#topic-input").value;
-  let languageInput = document.querySelector("#language-input").value;
+  let songLyrics = document.querySelector("#song-lyrics");
+  let topic = document.querySelector("#topic-input").value;
+  let language = document.querySelector("#language-input").value;
+  songLyrics.innerHTML = `<div class ="generating wavy">🎵 Generating song lyrics in ${language} about ${topic}. 🎵</div>`;
+  songLyrics.classList.remove("hidden");
   api_key = "58e244e95c3e78eb13e0ffotadf7c1b8";
   context =
     "You are an expert lyricist and poet, you can write in any language, real or fictional. Write a song in very short verses, add a <br /> between verses, it must be between four and twenty verses in length. All verses must be of similar lengths to fit with the music. The refrain must be in <strong></strong>. Follow user instructions to generate the lyrics. The song must have a title as an <h3 class='song-title'></h3> element at the top. Sign it '- SheCodes AI' at the end in <em class='signature'></em>";
-  prompt = `User Instructions: Generate song lyrics in ${languageInput} about ${topicInput}.`;
+  prompt = `User Instructions: Generate song lyrics in ${language} about ${topic}.`;
   api_url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
   axios.get(api_url).then(displayLyrics);
 }
